ca2a3f377e901fe6c18944fb3f93ba9afc075cba
alithethird <39213991+alithethird@users.noreply.github.com>
2020-06-16 20:14:58 +0300

    Update and rename takemcl.sh to showmcl.sh

---
+++ showmcl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/.komodo/src/komodo-cli -ac_name=MCL validateaddress RCMHJFXaw4gZUqUchRdPEyaepQ7EQLUJYP
--- takemcl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./komodo-cli -ac_name=MCL importprivkey Urwd8NXuh75xSmctKMZVb4seZ9fhB3tkawtn6vDmDJjbA4ithYNn
