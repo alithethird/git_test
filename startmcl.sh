1d4d1138da424744597cf090a588e2c943b72efd
alithethird <39213991+alithethird@users.noreply.github.com>
2020-06-16 19:28:05 +0300

    Update startmcl.sh

--- startmcl.sh
+++ startmcl.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#!/bin/bash
 
 komodod -ac_name=MCL -ac_supply=2000000 -ac_cc=2 -addnode=37.148.210.158 -addnode=37.148.212.36 -addressindex=1 -spentindex=1 -ac_marmara=1 -ac_staked=75 -ac_reward=3000000000 -pubkey=03123fac6ec4115d7acc3b40cf3e9870fce71fee061b39fb022dc84bdca4c013f0
